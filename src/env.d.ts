9056918a0577eadb245c702524431f5af95de2d8
Steve Roberts <steve@shadowcomputers.co.uk>
2026-06-08 12:07:16 +0100

    feat: add social media links and styles to footer
    Signed-off-by: Steve Roberts <steve@shadowcomputers.co.uk>

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 
 interface ImportMetaEnv {
-  readonly PUBLIC_GA_MEASUREMENT_ID?: string;
-  readonly PUBLIC_SITE_URL?: string;
-  readonly PUBLIC_CF_BEACON_TOKEN?: string;
-  readonly PUBLIC_TURNSTILE_SITE_KEY?: string;
+  readonly SITE?: string;
 }
 
 interface ImportMeta {
